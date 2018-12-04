c0e214ef1aa109aec5f414deb954df2b1a59ea0d
ああああああああ\Yayoi <no>
2018-12-03 17:22:20 -0800

    Initial Commit

---
+++ src/main/java/edu/averagejoe/averagejoezuul/AveragejoeZuulApplication.java
@@ -0,0 +1,12 @@
+package edu.averagejoe.averagejoezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AveragejoeZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AveragejoeZuulApplication.class, args);
+	}
+}
