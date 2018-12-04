6868900998ad7e5fb27016c71cc9359728ebbf3f
ああああああああ\Yayoi <no>
2018-12-03 17:37:55 -0800

    zuul

--- src/main/java/edu/averagejoe/averagejoezuul/AveragejoeZuulApplication.java
+++ src/main/java/edu/averagejoe/averagejoezuul/AveragejoeZuulApplication.java
@@ -1,12 +1,44 @@
 package edu.averagejoe.averagejoezuul;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class AveragejoeZuulApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AveragejoeZuulApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AveragejoeZuulApplication.class, args);
+
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        List<String> origins = new ArrayList<String>();
+        origins.add("*");
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(origins);
+        config.setAllowedHeaders(origins);
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
